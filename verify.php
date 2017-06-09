a8ad2ba8effa178c1e5027a25e48fe2ac164a033
prawitth <prawit.thammisorn@gmail.com>
2017-06-09 18:51:13 +0700

    Create verify.php

--- verify.php
+++ verify.php
@@ -2,6 +2,8 @@
 $access_token = 'AVfhYVHdIsNnjJJYcAwtwowcjjDZ6dZBYTlhuaqz8K+oBFKYQAbZChUjyi3mQitLF8k9ASoFn6baNpS9qRjK+F3jHZXWK+puyvXKVzUNv/8hR2sP2Go3N70j66TwlqwCYppViLrwh/pfY+mKLROGaQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:51vK0qDLySGq4sU';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
@@ -13,3 +15,4 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+ # http://username:password@proxyurl:port
