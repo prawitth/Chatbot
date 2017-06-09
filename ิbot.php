f4486a045b602254129ebdde82a4ae8474f82ddb
prawitth <prawit.thammisorn@gmail.com>
2017-06-09 17:56:03 +0700

    Create ิbot.php

---
+++ ิbot.php
@@ -0,0 +1,4 @@
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {AVfhYVHdIsNnjJJYcAwtwowcjjDZ6dZBYTlhuaqz8K+oBFKYQAbZChUjyi3mQitLF8k9ASoFn6baNpS9qRjK+F3jHZXWK+puyvXKVzUNv/8hR2sP2Go3N70j66TwlqwCYppViLrwh/pfY+mKLROGaQdB04t89/1O/w1cDnyilFU=}' \
+
